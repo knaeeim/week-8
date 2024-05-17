3db6db12f11b3bbda2193a3e06a7c698f4ccb6a2
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-05-17 19:29:16 +0530

    added Day-7

---
+++ Day-7/B_High_School_Become_Human.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+
+    double x, y, a, b; 
+    cin >> x >> y; 
+
+    a = y * log(x); 
+    b = x * log(y); 
+
+    if(a < b) cout << "<" << endl; 
+    else if(a > b) cout << ">" << endl; 
+    else cout << "=" << endl; 
+
+    return 0; 
+}
